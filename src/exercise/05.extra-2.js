dcb185ba5d848171c33bd07a7cb4f32f43b153fc
Rafael Santana <rafaelsantana3095@gmail.com>
2021-04-10 18:27:22 -0300

    feat: exercise 5.extra-2

---
+++ src/exercise/05.extra-2.js
@@ -0,0 +1,33 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import * as React from 'react'
+import '../box-styles.css'
+
+function Box({size, style, ...props}) {
+  return (
+    <div
+      className={`box box--${size}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...props}
+    />
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+    </div>
+  )
+}
+
+export default App
