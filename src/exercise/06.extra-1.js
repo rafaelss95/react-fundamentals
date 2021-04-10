4533219c9598853b090271e6253b4a481eebf649
Rafael Santana <rafaelsantana3095@gmail.com>
2021-04-10 17:10:27 -0300

    feat: exercise 6

--- src/exercise/06.extra-1.js
+++ src/exercise/06.extra-1.js
@@ -4,17 +4,18 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const usernameInputRef = React.useRef()
+
   function handleSubmit($event) {
     $event.preventDefault()
-    const username = $event.target.elements.usernameInput.value
-    onSubmitUsername(username)
+    onSubmitUsername(usernameInputRef.current.value)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" />
+        <input id="usernameInput" type="text" ref={usernameInputRef} />
       </div>
       <button type="submit">Submit</button>
     </form>
